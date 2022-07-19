5d3eb3ed1f2cefc8a3ee7d60925eb57b2a78927c
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-07-19 16:56:43 -0700

    commit tree 7c5653f66204ba1ce71fe3c12fde44962dc35957

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+saturdayFun();
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(outerParameter = "*"){
+    return function innerFunction(parameter = "special"){
+        return `You are ${outerParameter}${parameter}${outerParameter}!`
+    }
+}
+
+const innerFunction = wrapAdjective("!!!")
